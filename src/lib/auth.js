1a15f4eddc9736029f5c5af0c6a59d5e14f1d13f
Kazi Md Osman Goni <kaziosman873@gmail.com>
2026-05-22 22:56:49 +0600

    register error

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -6,10 +6,15 @@ import dns from "node:dns/promises";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
 const client = new MongoClient(process.env.MONGODB_URI);
-const db = client.db("SportNest");
+const db = client.db("sportnest");
 
 export const auth = betterAuth({
     baseURL: process.env.BETTER_AUTH_URL,
+    trustedOrigins: [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "https://assignment09-server-side.vercel.app",
+    ],
     database: mongodbAdapter(db, {
         client
     }),
