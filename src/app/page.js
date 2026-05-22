38fcb1c4965badf27ac75cda606ecf3e43f166ec
Kazi Md Osman Goni <kaziosman873@gmail.com>
2026-05-22 23:57:14 +0600

    home page added

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,7 @@ const HeroSection = () => {
 
       <div className="absolute inset-0 z-0">
         <Image 
-          src="https://images.unsplash.com/photo-1631548532844-7409538a1690?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" 
+          src="https://images.unsplash.com/photo-1612872087720-bb876e2e67d1?q=80&w=1170&auto=format&fit=crop" 
           alt="Sport-Nest Indoor Court"
           fill
           className="object-cover object-center"
