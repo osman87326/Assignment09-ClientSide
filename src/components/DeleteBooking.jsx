d2b12142e58d9f8d8c5a7e09e48ce4442ac0b6ae
Kazi Md Osman Goni <kaziosman873@gmail.com>
2026-05-22 15:02:54 +0600

    delete page

---
+++ src/components/DeleteBooking.jsx
@@ -0,0 +1,63 @@
+"use client";
+import { bookingDelete } from "@/lib/action";
+import { AlertDialog, Button } from "@heroui/react";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { toast } from "react-hot-toast";
+import { useRouter } from 'next/navigation'
+import { authClient } from "@/lib/auth-client";
+
+const DeleteFacility = ({ id, name }) => {
+  const router = useRouter()
+
+  async function handleDlt() {
+    const { data: tokenData} = await authClient.token()
+    const token = tokenData?.token; 
+    const result = await bookingDelete({ id, name, token })
+    if (result.success) {
+      toast.error(`${name} is removed`)
+      router.push('/myBookings')
+      router.refresh()
+    } else {
+      toast.error('Something went wrong!')
+    }
+  }
+
+  return (
+    <div>
+      <AlertDialog>
+        <Button variant="danger"><FaRegTrashAlt /> Delete</Button>
+        <AlertDialog.Backdrop>
+          <AlertDialog.Container>
+            <AlertDialog.Dialog className="sm:max-w-100">
+              <AlertDialog.CloseTrigger />
+
+              <AlertDialog.Header>
+                <AlertDialog.Icon status="danger" />
+                <AlertDialog.Heading>Delete Item permanently?</AlertDialog.Heading>
+              </AlertDialog.Header>
+
+              <AlertDialog.Body>
+                <p>
+                  This will permanently delete <strong>{name}</strong> and all of its
+                  data. This action cannot be undone.
+                </p>
+              </AlertDialog.Body>
+
+              <AlertDialog.Footer>
+                <Button slot="close" variant="tertiary">
+                  Cancel
+                </Button>
+                <Button onPress={handleDlt} slot="close" variant="danger">
+                  Delete Booking item
+                </Button>
+              </AlertDialog.Footer>
+            </AlertDialog.Dialog>
+          </AlertDialog.Container>
+        </AlertDialog.Backdrop>
+      </AlertDialog>
+
+    </div>
+  );
+};
+
+export default DeleteFacility;
